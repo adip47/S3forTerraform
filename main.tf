60a07052aec0eda4613fe96467cf69cfa99b75fe
ADITYA  PAWAR <64777945+adip47@users.noreply.github.com>
2024-02-28 14:33:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,11 @@ resource "aws_s3_bucket" "test-bucket-sm" {
         bucket = "test-bucket-sm-${var.env_name}"
         acl = "private"
 }
+resource "aws_s3_bucket" "test-bucket-sm-bkt" {
+        bucket = "test-bucket-sm-bkt-${var.env_name}"
+        acl = "private"
+}
+resource "aws_s3_bucket" "test-bucket-sm-bkt-test" {
+        bucket = "test-bucket-sm-bkt--test-${var.env_name}"
+        acl = "private"
+}
